430919372345c3004ff29458f99223c4da41b3d6
xiping <xiping.yan@intel.com>
2022-01-17 15:06:12 +0800

    add save excel test.

--- parse_test_result.py
+++ parse_test_result.py
@@ -1,6 +1,9 @@
 import sys
 import os.path as p
 import argparse
+# importing xlwt module
+import xlwt
+from xlwt import Workbook
 
 g_input_name = ""
 g_output_name = ""
@@ -45,6 +48,7 @@ def parse_log():
     for line in Lines:
         count += 1
 
+        # Compare each line flag.
         if FlagProcessComs == 1:
             if line[0:11] == "Test case #":
                 Test_One_Com_Num += 1
@@ -94,8 +98,26 @@ def export_excel(AllRslt):
        print("  {}: {}/{}".format(r["com"], r["err_num"], r["case_num"]))
     
     print("Test_Coms_Num: {}, {}/{}".format(TotalComs, TotalErrNum, TotalCases))
-
+  
+    # Workbook is created
+    wb = Workbook()
     
+    # add_sheet is used to create sheet.
+    sheet1 = wb.add_sheet('Sheet 1')
+    
+    sheet1.write(1, 0, 'ISBT DEHRADUN')
+    sheet1.write(2, 0, 'SHASTRADHARA')
+    sheet1.write(3, 0, 'CLEMEN TOWN')
+    sheet1.write(4, 0, 'RAJPUR ROAD')
+    sheet1.write(5, 0, 'CLOCK TOWER')
+    sheet1.write(0, 1, 'ISBT DEHRADUN')
+    sheet1.write(0, 2, 'SHASTRADHARA')
+    sheet1.write(0, 3, 'CLEMEN TOWN')
+    sheet1.write(0, 4, 'RAJPUR ROAD')
+    sheet1.write(0, 5, 'CLOCK TOWER')
+    
+    global g_output_name
+    wb.save(g_output_name)
 
 if __name__ == "__main__":
    parse_param()
